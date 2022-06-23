5193f14bff5215cd38b761f81f41c6993e5561cf
umenokoudai <umenokoudai0805@gmail.com>
2022-06-23 18:10:04 +0900

    デバッグ

--- Assets/Matubara/Script/LoadScene.cs
+++ Assets/Matubara/Script/LoadScene.cs
@@ -6,20 +6,20 @@ using UnityEngine.SceneManagement;
 public class LoadScene : MonoBehaviour
 {
     [Header("ˆÚ“®‚µ‚½‚¢ƒV[ƒ“–¼‚ğ“ü—Í")] [SerializeField] string _sceneName;
-    public void ChangeScene()
+    public void ChangeScene(string _sceneName)
     {
         Debug.Log("Click");
         SceneManager.LoadScene(_sceneName);
     }
 
-    public void Audio()
+    public void Audio(string _sceneName)
     {
         StartCoroutine(Enumerator());
 
         IEnumerator Enumerator()
         {
             yield return new WaitForSeconds(1.0f);
-            ChangeScene();
+            ChangeScene(_sceneName);
         }
     }
 }
