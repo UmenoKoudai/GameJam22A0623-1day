a053319ff3ea2a3b493b60b0142e9502d39dbc91
SatouTappei <stutpi.signup@gmail.com>
2022-06-23 12:28:13 +0900

    SceneManagerの名前をGameSceneManagerに変更

--- Assets/Sato/GameSceneManager.cs
+++ Assets/Sato/GameSceneManager.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class SceneManager : MonoBehaviour
+
+public class GameSceneManager : MonoBehaviour
 {
     // /////////////////////////////////
     /* ゲーム本編のシーンマネージャー */
