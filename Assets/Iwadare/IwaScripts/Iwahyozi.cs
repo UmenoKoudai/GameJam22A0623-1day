92c5ba754fae7d66322f237a332ec36e5bccf265
umenokoudai <umenokoudai0805@gmail.com>
2022-06-23 13:33:10 +0900

    キャラのアニメーション、ボタン一部変更

---
+++ Assets/Iwadare/IwaScripts/Iwahyozi.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Iwahyozi : MonoBehaviour
+{
+    [SerializeField] GameObject _resalt;
+    [SerializeField] GameObject _win;
+    [SerializeField] GameObject _lose;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
